4242755e6d873819e205852ce2b5e80bc8ee9005
Danylov Oleksandr <ol.danylov@gmail.com>
2020-12-22 11:46:55 +0200

    First commit

---
+++ Assets/Challenge 1/Scripts/PropellerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerControllerX : MonoBehaviour
+{
+    public float rotationSpeed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin the plane’s propeller
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // We'll move the vehicle forward along Z axis
+        // transform.Translate(0,0,1);
+        transform.Translate(speed     * forwardInput    * Time.deltaTime * Vector3.forward);
+        // transform.Translate(turnSpeed * horizontalInput * Time.deltaTime * Vector3.right);
+        // We'll rotate the vehicle along Y axis
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
